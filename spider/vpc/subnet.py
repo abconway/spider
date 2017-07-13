3838db4ed62dfa49e82e8eb509832f7a6bd1785b
abconway <abconway2@gmail.com>
2017-07-13 16:44:23 -0400

    add some basic descibe methods for vpc and subnet

--- spider/vpc/subnet.py
+++ spider/vpc/subnet.py
@@ -30,6 +30,26 @@ def create_subnet(name, vpc_id, availability_zone, cidr_block, environment='deve
     return subnet
 
 
+def describe_subnet(subnet_id):
+    ec2 = boto3.resource('ec2')
+
+    subnet = ec2.Subnet(subnet_id)
+
+    return {
+        'subnet_id': subnet.subnet_id,
+        'vpc_id': subnet.vpc_id,
+        'assign_ipv6_address_on_creation': subnet.assign_ipv6_address_on_creation,
+        'availability_zone': subnet.availability_zone,
+        'available_ip_address_count': subnet.available_ip_address_count,
+        'cidr_block': subnet.cidr_block,
+        'default_for_az': subnet.default_for_az,
+        'ipv6_cidr_block_association_set': subnet.ipv6_cidr_block_association_set,
+        'map_public_ip_on_launch': subnet.map_public_ip_on_launch,
+        'state': subnet.state,
+        'tags': subnet.tags,
+    }
+
+
 def delete_subnet(subnet_id):
     ec2_client = boto3.client('ec2')
 
--- spider/vpc/vpc.py
+++ spider/vpc/vpc.py
@@ -2,6 +2,7 @@ import boto3
 from botocore.exceptions import ClientError
 
 from .nat_gateway import wait_for_nat_gateway_to_delete
+from .subnet import describe_subnet
 
 
 def create_vpc(name, cidr_block, environment='development'):
@@ -28,6 +29,28 @@ def create_vpc(name, cidr_block, environment='development'):
 
     return vpc
 
+
+def describe_vpc(vpc_id, deep=False):
+    ec2 = boto3.resource('ec2')
+
+    vpc = ec2.Vpc(vpc_id)
+
+    vpc_data = {
+        'vpc_id': vpc.vpc_id,
+        'cidr_block': vpc.cidr_block,
+        'dhcp_options_id': vpc.dhcp_options_id,
+        'instance_tenancy': vpc.instance_tenancy,
+        'ipv6_cidr_block_association_set': vpc.ipv6_cidr_block_association_set,
+        'is_default': vpc.is_default,
+        'state': vpc.state,
+        'tags': vpc.tags,
+    }
+    if not deep:
+        return vpc_data
+
+    vpc_data['subnets'] = [describe_subnet(subnet.id) for subnet in vpc.subnets.all()]
+
+    return vpc_data
 
 def delete_vpc(vpc_id, force=False):
     ec2_client = boto3.client('ec2')
